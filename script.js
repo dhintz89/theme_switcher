e9e50d944f1cb56d801405577c3ded1ab13cd2e9
dhintz <dhintz89@gmail.com>
2020-08-15 19:02:04 -0400

    convert to CSS variables and add more theme options

--- script.js
+++ script.js
@@ -9,23 +9,33 @@
 //     scriptTag.href = splitHref.join('/');
 // }
 
-function changeTheme() {
-    // grab the script that controls colors (either darkmode.css or lightmode.css)
-    const scriptTag = document.getElementById("colorScheme");
-    // break the href into array
-    const splitHref = scriptTag.href.split("/");
-    // case statement based on which button was clicked
-    switch(event.target.id) {
-        case "darkThemeBtn":
-            splitHref.splice(-1, 1, "darkmode.css");
-            break;
-        case "lightThemeBtn":
-            splitHref.splice(-1, 1, "lightmode.css");
-            break;
-        case "blueThemeBtn":
-            splitHref.splice(-1, 1, "bluetheme.css");
-            break;
-    }
-    // combine altered array back into string and set as new href
-    scriptTag.href = splitHref.join('/');
+// function changeTheme() {
+//     // grab the script that controls colors (either darkmode.css or lightmode.css)
+//     const scriptTag = document.getElementById("colorScheme");
+//     // break the href into array
+//     const splitHref = scriptTag.href.split("/");
+//     // case statement based on which button was clicked
+//     switch(event.target.id) {
+//         case "darkThemeBtn":
+//             splitHref.splice(-1, 1, "darkmode.css");
+//             break;
+//         case "lightThemeBtn":
+//             splitHref.splice(-1, 1, "lightmode.css");
+//             break;
+//         case "blueThemeBtn":
+//             splitHref.splice(-1, 1, "bluetheme.css");
+//             break;
+//     }
+//     // combine altered array back into string and set as new href
+//     scriptTag.href = splitHref.join('/');
+// }
+
+function apply() {
+    event.preventDefault();
+    // get variable name
+    const cssVar = event.target.className
+    // get new value
+    const input = event.target.dataset.value;
+    // set variable name to new value
+    document.documentElement.style.setProperty(`--${cssVar}`, input);
 }
