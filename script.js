f045fff4c0fbd6c9b9a4ae25037d2085cbb9a092
dhintz <dhintz89@gmail.com>
2020-08-15 16:54:02 -0400

    add stylesheet and toggle functionality for blue theme

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-function toggleDarkMode() {
+// function toggleDarkMode() {
+//     // grab the script that controls colors (either darkmode.css or lightmode.css)
+//     const scriptTag = document.getElementById("colorScheme");
+//     // break the href into array
+//     const splitHref = scriptTag.href.split("/");
+//     // determine if scheme is currently light or dark, then switch the value within href array
+//     splitHref[splitHref.length-1] == "darkmode.css" ? splitHref.splice(-1, 1, "lightmode.css") : splitHref.splice(-1, 1, "darkmode.css");
+//     // combine altered array back into string and set as new href
+//     scriptTag.href = splitHref.join('/');
+// }
+
+function changeTheme() {
     // grab the script that controls colors (either darkmode.css or lightmode.css)
     const scriptTag = document.getElementById("colorScheme");
     // break the href into array
     const splitHref = scriptTag.href.split("/");
-    // determine if scheme is currently light or dark, then switch the value within href array
-    splitHref[splitHref.length-1] == "darkmode.css" ? splitHref.splice(-1, 1, "lightmode.css") : splitHref.splice(-1, 1, "darkmode.css");
+    // case statement based on which button was clicked
+    switch(event.target.id) {
+        case "darkThemeBtn":
+            splitHref.splice(-1, 1, "darkmode.css");
+            break;
+        case "lightThemeBtn":
+            splitHref.splice(-1, 1, "lightmode.css");
+            break;
+        case "blueThemeBtn":
+            splitHref.splice(-1, 1, "bluetheme.css");
+            break;
+    }
     // combine altered array back into string and set as new href
     scriptTag.href = splitHref.join('/');
 }
